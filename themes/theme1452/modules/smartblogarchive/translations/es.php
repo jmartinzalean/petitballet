aff0824c1a680d138a06eedc61a6227c59cda430
jmtomaszafra <jmtomaszafra@gmail.com>
2018-05-09 19:26:27 +0200

    footer y actualizo mi entorno con pro

---
+++ themes/theme1452/modules/smartblogarchive/translations/es.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{smartblogarchive}theme1452>smartblogarchive_da1573901cef12a84b936db2e4a45142'] = 'Archivo de Smart Blog';
+$_MODULE['<{smartblogarchive}theme1452>smartblogarchive_9e5d3342fda20f4bdbe8009c8b10fb49'] = 'El Más Poderoso Módulo de Archivo de Blogs de Presta shop – por smartdatasoft';
+$_MODULE['<{smartblogarchive}theme1452>smartblogarchive_fa214007826415a21a8456e3e09f999d'] = '¿Estás seguro de querer borrar tus datos?';
+$_MODULE['<{smartblogarchive}theme1452>smartblogarchive_ca18b5fea5dcf5c45e7af343d97e774c'] = 'Archivo del Blog';
---
+++ themes/theme1452/modules/smartbloghomelatestnews/translations/es.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartbloghomelatestnews_660a5e003bce0177d7aa7b39c1c0df5c'] = 'Últimas Noticias de Smart Blog';
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartbloghomelatestnews_a14895030612094493056c982a7089c9'] = 'Etiquetas del Más Poderoso Módulo de Blogs de Presta shop – por smartdatasoft';
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartbloghomelatestnews_fa214007826415a21a8456e3e09f999d'] = '¿Estás seguro de querer borrar tus datos?';
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartbloghomelatestnews_21ee0d457c804ed84627ec8345f3c357'] = 'La configuración ha sido actualizada con éxito.';
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartbloghomelatestnews_c54f9f209ed8fb4683e723daa4955377'] = 'Configuración General';
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartbloghomelatestnews_ea2766a70e61de99505624aa16055d4f'] = 'Número de Publicaciones para Mostrar en Últimas Noticias';
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartbloghomelatestnews_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
+$_MODULE['<{smartbloghomelatestnews}theme1452>smartblog_latest_news_decbe415d8accca7c2c16d48a79ee934'] = 'Leer Más';
